b032340f3f85d2a9e2c5be78f76c176ce5b69c19
Pradeep7a7 <76582258+Pradeep7a7@users.noreply.github.com>
2022-11-12 13:23:36 +0530

    New Change.

--- Source/Basics&Locators/src/Basic1.java
+++ Source/Basics&Locators/src/Basic1.java
@@ -3,7 +3,7 @@ public class Basic1 {
 
 	public static void main(String[] args) {
 		// For Printing in Output
-		System.out.println("Basic Test from Clones in new Branchs from Desktop App");
+		System.out.println("Basic Test");
 		
 		int a=3;
 		int b=2;
