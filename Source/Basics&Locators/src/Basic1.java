a30cbbcebe891535eda149ee5adcb45fd1e1c937
Pradeep7a7 <76582258+Pradeep7a7@users.noreply.github.com>
2022-11-12 15:54:24 +0530

    GIT Upload

--- Source/Basics&Locators/src/Basic1.java
+++ Source/Basics&Locators/src/Basic1.java
@@ -3,7 +3,7 @@ public class Basic1 {
 
 	public static void main(String[] args) {
 		// For Printing in Output
-		System.out.println("Test Branch");
+		System.out.println("Master Branch");
 		
 		int a=3;
 		int b=2;
